9bfc0de95b51a7338a8ce357427bfbc96e34ff07
vzivkovic <vzivkovic@me.com>
2019-05-05 20:50:02 +0200

    Add env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: 'b6907d289e10d714a6e88b30761fae22',
+  baseUrl: 'http://'
+
 };
 
 /*
@@ -13,4 +16,6 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.oi
+// https://openweathermap.org/data/2.5/weather?lat=43.32472&lon=21.90333&appid=b6907d289e10d714a6e88b30761fae22
+// https://openweathermap.org/data/2.5/weather?q=Nis,RS&appid=b6907d289e10d714a6e88b30761fae22

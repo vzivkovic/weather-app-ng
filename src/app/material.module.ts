dd6ab8bb88e20924e468949360e9678600ea2556
vzivkovic <vzivkovic@me.com>
2019-08-06 23:38:35 +0200

    Implementacija matirijala u temu

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule, MatIconModule} from '@angular/material';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, MatButtonModule, MatToolbarModule, MatIconModule
+  ],
+  exports: [
+    CommonModule, MatButtonModule, MatToolbarModule, MatIconModule
   ]
 })
 export class MaterialModule {

5845f62472369d7ec58c2897aec5df0455b0addd
vzivkovic <vzivkovic@me.com>
2019-08-13 08:47:26 +0200

    Materialt intall

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,15 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule, MatIconModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, MatButtonModule, MatToolbarModule, MatIconModule
+    CommonModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule,
   ],
   exports: [
-    CommonModule, MatButtonModule, MatToolbarModule, MatIconModule
+    CommonModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule,
   ]
 })
 export class MaterialModule {
